9956d2e2b7aee16bf4702e101a3fdaae40ef05ec
Matthias Hryniszak <padcom@gmail.com>
2018-11-22 23:20:19 +0100

    Switched to graphql-yoga and added an Address relation to Person

--- index.js
+++ index.js
@@ -3,35 +3,8 @@
 // Example query using cURL
 // curl -X POST --data '{ "query": "{ people { id, name, age } }" }' --header "Content-Type: application/json" localhost:3000/graphql
 
-const { buildSchema } = require('graphql')
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
+const { GraphQLServer } = require('graphql-yoga')
 const uuid = require('uuid')
-
-const schema = buildSchema(`
-  type Person {
-    id: ID!
-    name: String!
-    age: Int!
-  }
-
-  type Query {
-    hello: String
-    rollDice(num: Int!, sides: Int): [Int]
-    person(id: Int!): Person
-    people: [Person!]
-  }
-
-  input PersonInput {
-    name: String
-    age: Int
-  }
-
-  type Mutation {
-    createPerson(name: String!, age: Int!): Person!
-    updatePerson(id: Int!, input: PersonInput!): Person!
-  }
-`)
 
 class Person {
   constructor(id, name, age) {
@@ -45,37 +18,87 @@ const people = {
   1: new Person(1, 'John Doe', 43),
 }
 
-const root = {
-  hello() {
-    return 'Hello, world!'
-  },
-  rollDice({ num, sides = 6 }) {
-    const result = []
-    for (let i = 0; i < num; i++) {
-      result.push(Math.round(Math.random() * sides))
-    }
-    return result
-  },
-  person({ id }) {
-    return people[id]
-  },
-  people() {
-    return Object.keys(people).map(id => people[id])
-  },
-  createPerson({ name, age }) {
-    const person = new Person(uuid(), name, age)
-    people[person.id] = person
-
-    return person
-  },
-  updatePerson({ id, input }) {
-    if (!people[id]) throw new Error(`No person with id ${id}`)
-    if (input.name) people[id].name = input.name
-    if (input.age) people[id].age = input.age
-    return people[id]
+class Address {
+  constructor(personId, city, street) {
+    this.personId = personId
+    this.city = city
+    this.street = street
   }
 }
 
-const app = express()
-app.use('/graphql', graphqlHTTP({ schema, rootValue: root, graphiql: true }))
-app.listen(3000, () => { console.log('Server listening on port 3000') })
+const addresses = [
+  new Address(1, 'New York', '1st street')
+]
+
+const helloResolvers = {
+  Query: {
+    hello() {
+      return 'Hello, world!'
+    },
+  }
+}
+
+const diceResolvers = {
+  Query: {
+    rollDice(_, { num, sides = 6 }) {
+      const result = []
+      for (let i = 0; i < num; i++) {
+        result.push(Math.round(Math.random() * sides))
+      }
+      return result
+    },
+  }
+}
+
+const personResolvers = {
+  Query: {
+    person(_, { id }) {
+      return people[id]
+    },
+    people() {
+      return Object.keys(people).map(id => people[id])
+    },
+  },
+  Mutation: {
+    createPerson(_, { name, age }) {
+      const person = new Person(uuid(), name, age)
+      people[person.id] = person
+
+      return person
+    },
+    updatePerson(_, { id, name, age }) {
+      if (!people[id]) throw new Error(`No person with id ${id}`)
+      if (name) people[id].name = name
+      if (age) people[id].age = age
+
+      return people[id]
+    },
+    addPersonAddress(_, { personId, city, street } = {}) {
+      const person = people[personId]
+      if (!person) throw new Error(`Person with ID=${personId} not found`)
+      const address = new Address(personId, city, street)
+      addresses.push(address)
+      return address
+    }
+  },
+
+  Person: {
+    addresses(person) {
+      return addresses.filter(address => address.personId == person.id)
+    },
+  },
+}
+
+const addressResolvers = {
+  Address: {
+    person(address) {
+      return people[address.personId]
+    },
+  },
+}
+
+
+const resolvers = [ helloResolvers, diceResolvers, personResolvers, addressResolvers ]
+
+const server = new GraphQLServer({ typeDefs: './schema.graphql', resolvers })
+server.start(() => console.log('Server listening on port 4000'))
